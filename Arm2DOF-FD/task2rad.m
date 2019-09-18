104e668ccc48fe80c673188ca8ce39409bcc8ec5
leonheuler <ironfelix1926@gmail.com>
2019-09-18 09:23:21 +0300

    App added

---
+++ Arm2DOF-FD/task2rad.m
@@ -0,0 +1,12 @@
+function taskRad = task2rad(taskDegrees)
+
+
+
+
+    taskRad(1,:) = taskDegrees(1,:)*(pi/180);
+    taskRad(2,:) = taskDegrees(2,:)*(pi/180);
+    taskRad(3,:) = taskDegrees(3,:);
+    
+
+end
+
