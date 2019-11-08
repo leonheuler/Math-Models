cacebdfe972eb07b556c80c16e8deed57ad20d26
Leonheuler <ironfelix1926@gmail.com>
2019-11-08 17:02:26 +0300

    upd

--- Ex3DOF/Equations2DoF.m
+++ Ex3DOF/Equations2DoF.m
@@ -1,6 +1,8 @@
 clear all
 
 syms L1 L2 L3 m1 m2 m3 g
+
+
 syms t real
 syms q1(t) 
 syms q2(t) 
@@ -32,34 +34,57 @@ P = P1 + P2;
 
 % Lagrangian
 L = K - P;
-
-
-% Partial derivatives of Kinetic energy 
-syms dummy
-dLq1dot = subs(diff(subs(L, diff(q1,t),dummy),dummy),dummy,diff(q1,t));
-dLq2dot = subs(diff(subs(L, diff(q2,t),dummy),dummy),dummy,diff(q2,t));
-
-% Partial derivatives of Lagrangian
-dLq1 = subs(diff(subs(L, q1, dummy),dummy),dummy, q1);
-dLq2 = subs(diff(subs(L, q2, dummy),dummy),dummy, q2);
-
-% Lagrange Differential equation
-diffEq1 = diff(dLq1dot,t) - dLq1;
-diffEq2 = diff(dLq2dot,t) - dLq2;
-
-% diffEq variables
 variables = { q1, q2,  diff(q1,t), diff(q2,t),  diff(q1,t,2), diff(q2,t,2)  };
-
-% desired variables 
 syms q1 q2 q1dot q2dot q1ddot q2ddot
 variablesshort = { q1, q2, q1dot, q2dot, q1ddot, q2ddot  };
+L = subs(L, variables, variablesshort);
 
-% Lagrange differential equations with desired variables
-DiffEq1 = subs(diffEq1, variables, variablesshort);
-DiffEq2 = subs(diffEq2, variables, variablesshort);
+dLq1dot = diff(L, q1dot);
+dLq2dot = diff(L, q2dot);
 
-eq1 = simplify(DiffEq1);
-eq2 = simplify(DiffEq2);
+dLq1 = diff(L, q1);
+dLq2 = diff(L, q2);
+
+dLq1dot = subs(dLq1dot, variablesshort, variables);
+dLdtq1dot = diff(dLq1dot, t);
+dLdtq1dot = subs(dLdtq1dot, variables, variablesshort);
+
+dLq2dot = subs(dLq2dot, variablesshort, variables);
+dLdtq2dot = diff(dLq2dot, t);
+dLdtq2dot = subs(dLdtq2dot, variables, variablesshort);
+
+eq1 = dLdtq1dot - dLq1;
+eq2 = dLdtq2dot - dLq2;
+
+eq1 = simplify(eq1);
+eq2 = simplify(eq2);
+
+% Partial derivatives of Kinetic energy 
+% syms dummy
+% dLq1dot = subs(diff(subs(L, diff(q1,t),dummy),dummy),dummy,diff(q1,t));
+% dLq2dot = subs(diff(subs(L, diff(q2,t),dummy),dummy),dummy,diff(q2,t));
+% 
+% % Partial derivatives of Lagrangian
+% dLq1 = subs(diff(subs(L, q1, dummy),dummy),dummy, q1);
+% dLq2 = subs(diff(subs(L, q2, dummy),dummy),dummy, q2);
+% 
+% % Lagrange Differential equation
+% diffEq1 = diff(dLq1dot,t) - dLq1;
+% diffEq2 = diff(dLq2dot,t) - dLq2;
+% 
+% % diffEq variables
+% variables = { q1, q2,  diff(q1,t), diff(q2,t),  diff(q1,t,2), diff(q2,t,2)  };
+% 
+% % desired variables 
+% syms q1 q2 q1dot q2dot q1ddot q2ddot
+% variablesshort = { q1, q2, q1dot, q2dot, q1ddot, q2ddot  };
+% 
+% % Lagrange differential equations with desired variables
+% DiffEq1 = subs(diffEq1, variables, variablesshort);
+% DiffEq2 = subs(diffEq2, variables, variablesshort);
+% 
+% eq1 = simplify(DiffEq1);
+% eq2 = simplify(DiffEq2);
 
 % Gathering elements of matrix M from equations
 tm11 = children(collect(eq1, q1ddot)); 
@@ -78,34 +103,35 @@ M = [ M11 M12;
 
 % Gathering elements of matrix V from equations
 
-tv11 = children(collect(eq1, q1dot)); 
-tv12 = children(collect(eq1, q2dot));
-tv21 = children(collect(eq2, q1dot)); 
-tv22 = children(collect(eq2, q2dot));
-
-tv11f = factor(tv11(1), q1dot);
-tv12f = factor(tv12(1), q2dot);
-tv21f = factor(tv21(1), q1dot);
-tv22f = factor(tv22(1), q2dot);
-
-V11 = prod(tv11f(length(tv11f)-1));
-V12 = prod(tv12f(length(tv12f)-1));
-V21 = prod(tv21f(length(tv21f)-1));
-V22 = prod(tv22f(length(tv22f)-1));
-
- 
-% V11 = coeffs( expand(tv11(1)), q1dot);
-% V12 = coeffs( expand(tv12(1)), q2dot);
-% V21 = coeffs( expand(tv21(1)), q1dot);
-% V22 = coeffs( expand(tv22(1)), q2dot);
-
-
-
-V = [ V11 V12;
-      V21 V22 ];
+% tv11 = children(collect(eq1, q1dot)); 
+% tv12 = children(collect(eq1, q2dot));
+% tv21 = children(collect(eq2, q1dot)); 
+% tv22 = children(collect(eq2, q2dot));
+% 
+% tv11f = factor(tv11(1), q1dot);
+% tv12f = factor(tv12(1), q2dot);
+% tv21f = factor(tv21(1), q1dot);
+% tv22f = factor(tv22(1), q2dot);
+% 
+% V11 = prod(tv11f(length(tv11f)-1));
+% V12 = prod(tv12f(length(tv12f)-1));
+% V21 = prod(tv21f(length(tv21f)-1));
+% V22 = prod(tv22f(length(tv22f)-1));
+% 
+%  
+% % V11 = coeffs( expand(tv11(1)), q1dot);
+% % V12 = coeffs( expand(tv12(1)), q2dot);
+% % V21 = coeffs( expand(tv21(1)), q1dot);
+% % V22 = coeffs( expand(tv22(1)), q2dot);
+% 
+% 
+% 
+% V = [ V11 V12;
+%       V21 V22 ];
   
 % Solve system of L diff. equations relative to angular accelerations
-solution = solve(DiffEq1, DiffEq2,  q1ddot, q2ddot );
+% solution = solve(DiffEq1, DiffEq2,  q1ddot, q2ddot );
+solution = solve(eq1, eq2,  q1ddot, q2ddot );
 
 Q1DDOT = solution.q1ddot;
 Q2DDOT = solution.q2ddot;
